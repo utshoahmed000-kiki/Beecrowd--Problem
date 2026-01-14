861bdb978089fd9399663782ab7c992f7034ec40
utshoahmed000-kiki <utshoahmed000@gmail.com>
2026-01-14 22:24:03 +0600

    1005 - Average 1

---
+++ src/beecrowdProblem6.kt
@@ -0,0 +1,14 @@
+fun main (){
+    val A =readLine()!!.toDouble()
+    val B = readLine()!!.toDouble()
+
+    val weightA=3.5*A
+    val weightB=7.5*B
+
+    val average=(weightA+weightB)/11.00
+    println("MEDIA = %.5f".format(average))
+
+
+
+
+}
