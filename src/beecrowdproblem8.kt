1c25c130cd8664a566ae7b96614680d8d753d685
utshoahmed000-kiki <utshoahmed000@gmail.com>
2026-01-16 20:30:25 +0600

    beecrowd | 1007 Difference

---
+++ src/beecrowdproblem8.kt
@@ -0,0 +1,13 @@
+fun main (){
+
+    val A=readLine()!!.toInt()
+    val B=readLine()!!.toInt()
+    val C=readLine()!!.toInt()
+    val D=readLine()!!.toInt()
+
+
+    val abcd=A*B-C*D
+    println("DIFERENCA = $abcd")
+
+
+}
